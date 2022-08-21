92483857dec7e88c0e95c92dd6500b4adbae2ff2
Talha Tariq <talhacoder01@gmail.com>
2022-08-21 16:31:54 +0500

    Project Start

---
+++ app/src/main/java/haqnawaz/org/a20220815db/DBHelper.java
@@ -0,0 +1,95 @@
+package haqnawaz.org.a20220815db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String STUDENT_ID = "StudentID";
+    public static final String STUDENT_NAME = "StudentName";
+    public static final String STUDENT_ROLL = "StudentRollNumber";
+    public static final String STUDENT_ENROLL = "IsEnrolled";
+    public static final String STUDENT_TABLE = "StudentTable";
+
+
+
+    public DBHelper(@Nullable Context context) {
+        super(context, "MyDB.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createStudentTableStatement = "CREATE TABLE " + STUDENT_TABLE + "(" +
+                STUDENT_ID + " Integer PRIMARY KEY AUTOINCREMENT, " + STUDENT_NAME + " Text, "
+                + STUDENT_ROLL + " Int, " + STUDENT_ENROLL + " BOOL) ";
+        db.execSQL(createStudentTableStatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
+        onCreate(db);
+    }
+
+    public void addStudent(StudentModel STUDENTModel){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        //Hash map, as we did in bundles
+        ContentValues cv = new ContentValues();
+
+        cv.put(STUDENT_NAME, STUDENTModel.getName());
+        cv.put(STUDENT_ROLL, STUDENTModel.getRollNmber());
+        cv.put(STUDENT_ENROLL, STUDENTModel.isEnroll());
+        db.insert(STUDENT_TABLE, null, cv);
+        db.close();
+        //NullCoumnHack
+        //long insert =
+        //if (insert == -1) { return false; }
+        //else{return true;}
+    }
+
+    boolean updateStudent(int rollNum, String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(STUDENT_NAME, name);
+        cv.put(STUDENT_ROLL, rollNum);
+        return db.update(STUDENT_TABLE,cv,STUDENT_ROLL + "=" + rollNum,null)>0;
+    }
+
+
+    boolean deleteStudent(int rollNum){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(STUDENT_TABLE,STUDENT_ROLL + "=" + rollNum,null)>0;
+    }
+
+
+
+    public ArrayList<StudentModel> getAllStudents() {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + STUDENT_TABLE, null);
+
+        ArrayList<StudentModel> studentArrayList = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+
+                studentArrayList.add(new StudentModel(cursorCourses.getString(1),
+                        cursorCourses.getInt(2),
+                        cursorCourses.getInt(3) == 1 ? true : false));
+            } while (cursorCourses.moveToNext());
+
+        }
+        cursorCourses.close();
+        return studentArrayList;
+    }
+
+}
---
+++ app/src/main/java/haqnawaz/org/a20220815db/MainActivity.java
@@ -0,0 +1,103 @@
+package haqnawaz.org.a20220815db;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonAdd, buttonViewAll, updateButton,deleteButton;
+    TextView resultText;
+    EditText editName, editRollNumber;
+    Switch switchIsActive;
+    ListView listViewStudent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonViewAll = findViewById(R.id.buttonViewAll);
+        updateButton = findViewById(R.id.updateBtn);
+        editName = findViewById(R.id.editTextName);
+        editRollNumber = findViewById(R.id.editTextRollNumber);
+        switchIsActive = findViewById(R.id.switchStudent);
+        listViewStudent = findViewById(R.id.listViewStudent);
+        deleteButton = findViewById(R.id.deleteBtn);
+
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            StudentModel studentModel;
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    studentModel = new StudentModel(editName.getText().toString(), Integer.parseInt(editRollNumber.getText().toString()), switchIsActive.isChecked());
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                DBHelper dbHelper  = new DBHelper(MainActivity.this);
+                dbHelper.addStudent(studentModel);
+            }
+        });
+
+        buttonViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                List<StudentModel> list = dbHelper.getAllStudents();
+                ArrayAdapter arrayAdapter = new ArrayAdapter<StudentModel>
+                        (MainActivity.this, android.R.layout.simple_list_item_1,list);
+                listViewStudent.setAdapter(arrayAdapter);
+            }
+        });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                try {
+                    dbHelper.updateStudent(Integer.parseInt(editRollNumber.getText().toString()), editName.getText().toString());
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean result ;
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                if(!editRollNumber.getText().toString().isEmpty()){
+                    result =   dbHelper.deleteStudent(Integer.parseInt(editRollNumber.getText().toString()));
+                    if(result){
+                        editName.setText("");
+                        editRollNumber.setText("");
+                        resultText.setText("Record Deleted");
+                    }
+                    else{
+                        resultText.setText("No Record Found");
+                    }
+                }
+                else{
+                    resultText.setText("Please Enter Correct Roll Number");
+                }
+            }
+        });
+
+
+    }
+}
